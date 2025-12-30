e6e5ffeb78c3b5971e075f58102393587e8f6011
hogewar <hogewar@gmail.com>
2025-12-30 18:30:37 +0800

    Fix CORS - add FRONTEND_URL from environment variable

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => array_filter([
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        env('FRONTEND_URL', ''),
+    ]),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
